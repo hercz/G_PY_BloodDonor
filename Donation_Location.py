f862302f8d8029155e6b04f50a66567a6907251a
Papuss <mrpapsu@gmail.com>
2015-10-27 14:46:08 +0100

    Donation Loaction vaza elso verzio

--- Donation_Location.py
+++ Donation_Location.py
@@ -1,4 +1,49 @@
 __author__ = 'Gazdik_Zsolt'
 
+class User_data(object):
 
-print("ASDASDASDASD")
+    def __init__(self):
+        pass
+
+    #Inputs, gets
+
+    def get_Date_of_Event(self):
+        pass
+
+    def get_Start_Time(self):
+        pass
+
+    def get_End_Time(self):
+        pass
+
+    def get_Zip_Code(self):
+        pass
+
+    def get_City(self):
+        pass
+
+    def get_Adress(self):
+        pass
+
+    def get_Beds_Available(self):
+        pass
+
+    def get_Planned_Donor_Number(self):
+        pass
+
+    # FUNCTIONS
+
+    def Registration_ten_days_before_date(self):
+        pass
+
+    def Datetime_Only_on_Weekdays(self):
+        pass
+
+    def Adress_Validation(self):
+        pass
+
+    def Calculate_Duration_in_Minutes(self):
+        pass
+
+    def Maximum_Donor_Number(self):
+        pass
