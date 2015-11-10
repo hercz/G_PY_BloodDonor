231e9b81e303e933b18407e634c46c9eff67b8d3
Herczku Misi <kmisku@gmail.com>
2015-11-10 19:23:17 +0100

    Donor_delete.py things open the Donor_Data.csv file check if contains the requested id

--- Donor_delete.py
+++ Donor_delete.py
@@ -1,25 +1,52 @@
-__author__ = 'herczkumihalybalazs'
+import csv
 
-id = 1
+class DeleteDonor(object):
 
-# file = open("./Data/Donor_Data.csv", "r+")
-# lines = file.readlines()
-# file.seek(0)
-# for i in lines:
-#     if i != id:
-#         file.write(i)
-# file.truncate()
-# file.close()
+    def __init__(self):
+        self.get_delete_id()
 
 
 
+    def get_delete_id(self):
+        delete_id = ""
+        delete_id = input("Please write here the person's ID which you want to delete from the database: ")
+        with open("./Data/Donor_Data.csv", newline="") as file:
+            reader = csv.reader(file, delimiter=",")
+            ids = []
+            for row in reader:
+                ids.append(row[3])
+            if delete_id not in str(ids):
+                    print("This ID is not in the database!")
+            else:
+                pass
 
 
-file = open("./Data/Donor_Data.csv", "r")
-lines = file.readlines()
-file.close()
 
-for i in lines:
-    print(i)
+if __name__ == '__main__':
+    DeleteDonor()
+#
+# id = "7"
+#
+# with open("./Data/Donor_Data.csv", newline="") as file:
+#     reader = csv.reader(file, delimiter=",")
+#     ids = []
+#     for row in reader:
+#         ids.append(row[3])
+#     if id not in str(ids):
+#             print("This ID is not in the database!")
+#     else:
+#         pass
+#     print(ids)
 
-file = open("./Data/Donor_Data.csv", "r+")
+#row_print
+# with open("./Data/Donor_Data.csv", newline="") as file:
+#     Reader = csv.reader(file, delimiter=",")
+#     for row in Reader:
+#         print(row[3])
+#mainbe
+     # if Picked_Option() == "3":
+     #    import Donor_delete as deleter
+     #    print_separator_line()
+     #    print("Welcome in the donor delete application!")
+     #    print()
+     #    deleter = deleter.DeleteDonor()
