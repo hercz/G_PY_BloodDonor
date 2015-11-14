b7189a52cef6c378daa783c2ce5bc0670955eafa
Herczku Misi <kmisku@gmail.com>
2015-11-14 19:12:37 +0100

    Location and User delete proba

--- Location_delete.py
+++ Location_delete.py
@@ -1,27 +1,64 @@
+
 import csv
 import Menu
 
-reload_list = []
+class DeleteLocation(object):
 
-location_id_to_delete = ""
-while location_id_to_delete == "":
-    location_id_to_delete = input("Please enter the line ID to delete: ")
-    if not location_id_to_delete.isdigit():
-        print("Your input must be a Number")
-        location_id_to_delete = ""
+    id_to_delete = ""
 
-with open("./Data/Location_Data.csv", "r") as TextIn:
-    csv_text = csv.reader(TextIn)
-    record_list = list(csv_text)
-    for line in record_list:
-        if not location_id_to_delete == line[0]:
-            reload_list.append(line)
-    for i in range(len(reload_list)):
-        if not i == 0:
-            reload_list[i][0] = i
+    def __init__(self):
+        self.get_delete_id()
 
-with open("./Data/Location_Data.csv", "w+") as NewTextData:
-    writer = csv.writer(NewTextData)
-    writer.writerows(reload_list)
+    def get_user_input(self):
+        user_input = input(print("Do you want to delete another location from the database? (Y/N)"))
+        if user_input.lower() == "y":
+            return self.get_delete_id()
+        elif user_input.lower() == "n":
+             Menu.Main_Menu()
+             Menu.Picked_option()
+        else:
+            print("Invalid input!")
+            self.get_user_input()
 
-Menu.ask_answer()
+
+    def delete_id(self):
+        location_data = open("./Data/Location_data.csv", "r")
+        lines = location_data.readlines()
+        location_data.close()
+        location_data = open("./Data/Location_data.csv", "w")
+        for line in lines:
+            if self.to_delete not in line[0]:
+                location_data.write(line)
+        location_data.close()
+        print("The location with {} Id is deleted".format(self.to_delete))
+        self.get_user_input()
+
+
+    def get_delete_id(self):
+        id_to_delete = ""
+        while id_to_delete == "":
+            id_to_delete = input("Please write here the location ID which you want to delete from the database: ")
+            self.to_delete = id_to_delete
+            if id_to_delete == "id":            # ATIRNI MERT MINDENT TOROL
+                print("Invalid ID format!")
+                self.get_user_input()
+            try:
+                with open("./Data/Location_Data.csv", newline="") as file:
+                    reader = csv.reader(file, delimiter=",")
+                    ids = []
+                    for row in reader:
+                        ids.append(row[0])
+                    if id_to_delete in str(ids):
+                        self.delete_id()
+                    else:
+                        print("This location ID is not in the database!")
+                        self.get_user_input()
+            except IndexError:
+                print("Your database is empty!")
+                print("Register some location before you want to delete them!")
+                Menu.Main_Menu()
+                Menu.Picked_option()
+
+
+if __name__ == '__main__':
+    DeleteLocation()
--- User_delete.py
+++ User_delete.py
@@ -9,24 +9,25 @@ class DeleteDonor(object):
         self.get_delete_id()
 
     def get_user_input(self):
-        user_input = input("Do you want to delete another donor from the database? (Y/N)")
+        user_input = input(print("Do you want to delete another donor from the database? (Y/N)"))
         if user_input.lower() == "y":
-            self.get_delete_id()
+            return self.get_delete_id()
         elif user_input.lower() == "n":
-            Menu.Main_Menu()
-            Menu.Picked_option()
+             Menu.Main_Menu()
+             Menu.Picked_option()
         else:
             print("Invalid input!")
             self.get_user_input()
 
 
     def delete_id(self):
-        donor_data = open("./Data/Donor_Data.csv", "r+")
+        donor_data = open("./Data/Donor_Data.csv", "r")
         lines = donor_data.readlines()
+        donor_data.close()
+        donor_data = open("./Data/Donor_Data.csv", "w")
         for line in lines:
             if self.to_delete not in line:
-                print("This ID is not found!")
-                self.get_delete_id()
+                donor_data.write(line)
         donor_data.close()
         print("The user with {} Id is deleted".format(self.to_delete))
         self.get_user_input()
