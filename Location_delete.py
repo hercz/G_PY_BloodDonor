aea78ca1ceacf5d73abf16e8f0800e15a78988dd
Herczku Misi <kmisku@gmail.com>
2015-11-15 15:06:53 +0100

    location_delete working

--- Location_delete.py
+++ Location_delete.py
@@ -10,7 +10,7 @@ class DeleteLocation(object):
         self.get_delete_id()
 
     def get_user_input(self):
-        user_input = input(print("Do you want to delete another location from the database? (Y/N)"))
+        user_input = input("Do you want to delete another location from the database? (Y/N)")
         if user_input.lower() == "y":
             return self.get_delete_id()
         elif user_input.lower() == "n":
@@ -50,25 +50,25 @@ class DeleteLocation(object):
         while id_to_delete == "":
             id_to_delete = input("Please write here the location ID which you want to delete from the database: ")
             self.to_delete = id_to_delete
-            if id_to_delete == "id":
-                print("Invalid ID format!")
-                self.get_user_input()
-            try:
-                with open("./Data/Location_Data.csv", newline="") as file:
-                    reader = csv.reader(file, delimiter=",")
-                    ids = []
-                    for row in reader:
-                        ids.append(row[0])
-                    if id_to_delete in str(ids):
-                        self.delete_id()
-                    else:
-                        print("This location ID is not in the database!")
-                        self.get_user_input()
-            except IndexError:
-                print("Your database is empty!")
-                print("Register some location before you want to delete them!")
-                Menu.Main_Menu()
-                Menu.Picked_option()
+        if id_to_delete == "id":
+            print("Invalid ID format!")
+            self.get_user_input()
+        try:
+            with open("./Data/Location_Data.csv", newline="") as file:
+                reader = csv.reader(file, delimiter=",")
+                ids = []
+                for row in reader:
+                    ids.append(row[0])
+                if id_to_delete in str(ids):
+                    self.delete_id()
+                else:
+                    print("This location ID is not in the database!")
+                    self.get_user_input()
+        except IndexError:
+            print("Your database is empty!")
+            print("Register some location before you want to delete them!")
+            Menu.Main_Menu()
+            Menu.Picked_option()
 
 
 if __name__ == '__main__':
