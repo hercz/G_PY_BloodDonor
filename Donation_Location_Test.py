b9875ee9f23fb5459651215122634156c884433e
benedekbalazs <benbal87@gmail.com>
2015-11-25 11:49:08 +0100

    Donation Location test begin again

--- Donation_Location_Test.py
+++ Donation_Location_Test.py
@@ -1,11 +1,21 @@
-__author__ = 'Gazdik_Zsolt'
-
 import unittest
+from Donation_Location2 import UserData
+
+__author__ = 'Stark_Industries'
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.user = UserData()
+
+    def test_if_date_is_empty(self):
+        result = self.user.check_date_of_event("")
+        self.assertEqual(False, result)
+
+    def test_if_date_is_empty(self):
+        result = self.user.check_date_of_event("")
+        self.assertEqual(False, result)
+
 
 
 if __name__ == '__main__':
